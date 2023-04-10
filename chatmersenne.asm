6f5dc7dba39e8a19065ef0d457611f345042ab13
pkos <peter_j_kos@yahoo.com>
2023-04-10 10:54:41 -0400

    ChatMersenne v0.6

--- chatmersenne.asm
+++ chatmersenne.asm
@@ -1,13 +1,22 @@
-mov eax, 0
-mov ebx, 1
-mov ecx, 1
+.model small
+.code
 
-next_term:
-    mov edx, aex   ; save previous term in edx
+main PROC
+
+     mov eax, 0
+     mov ebx, 1
+     mov ecx, 1
+
+  Next:
+    mov edx, eax   ; save previous term in edx
 	shl ebx, 1     ; multiply previous term by 2 using shift left
 	add eax, edx   ; add term before pervious to current term
-	add eax, 0x30 ; convert digit in exa to ASCII code character
-	mov eax, 02h    ; write to terminal in DOS
+;	add eax, 0x30  ; convert digit in exa to ASCII code character
+;	mov eax, 02h   ; write to terminal in DOS
 	inc ecx        ; increment counter
-;	cmp ecx, n     ; check if we've reached the desired number of terms (execute until ctrl^c)
-	jne next_term  ; if not, go to next term
+	cmp ecx, 10    ; check if we've reached the desired number of terms (execute until ctrl^c)
+	jne Next       ; if not, go to next term
+
+main ENDP
+
+END
